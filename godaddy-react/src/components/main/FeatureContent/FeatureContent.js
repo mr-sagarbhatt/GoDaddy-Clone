4cd3260bfa7703e84d7b35ba9c16c83c010fdb9e
sagar <bhattsagar112@gmail.com>
2022-03-10 10:57:11 +0530

    updated image links

--- godaddy-react/src/components/main/FeatureContent/FeatureContent.js
+++ godaddy-react/src/components/main/FeatureContent/FeatureContent.js
@@ -6,6 +6,9 @@ import {
 } from "../../../utils/Contentful";
 import Content from "./Content";
 import PageLoader from "../../Loader/PageLoader";
+import WordpressHostingImage from "../../../assets/images/MCXMO-63401-IN-HP-MWP-blade.jpg";
+import ProImage from "../../../assets/images/img-feat-pro.jpg";
+import ResellerImage from "../../../assets/images/img-feat-reseller.jpg";
 
 const FeatureContent = () => {
   // * STORE CONTENT
@@ -40,87 +43,90 @@ const FeatureContent = () => {
         <PageLoader></PageLoader>
       ) : (
         <div className='feature-content'>
-          {featuredContent.map((item, index) => (
+          {/* {featuredContent.map((item, index) => (
             <Content data={item} key={index}></Content>
-          ))}
-          {/* <div className='item'>
-        <div className='image'>
-          <img
-            className='img-fluid'
-            src='https://drive.google.com/uc?export=view&id=1ESwWU8z3Ioh9Z84wpXxk9W7vjuqbVufK'
-            alt='wordpress Hosting'
-          />
-        </div>
-        <div className='content-wrapper'>
-          <h2 className='title'>WordPress Hosting</h2>
-          <h3 className='sub-title'>Built for speed and security.</h3>
-          <div className='content'>
-            <p>
-              Get the most from your WordPress site with hosting designed to
-              perform. From automated updates and backups to industry-leading
-              load times, this is WordPress how it was meant to be.
-            </p>
+          ))} */}
+          <div className='item'>
+            <div className='image'>
+              <img
+                className='img-fluid'
+                src={WordpressHostingImage}
+                alt='wordpress Hosting'
+              />
+            </div>
+            <div className='content-wrapper'>
+              <h2 className='title'>WordPress Hosting</h2>
+              <h3 className='sub-title'>Built for speed and security.</h3>
+              <div className='content'>
+                <p>
+                  Get the most from your WordPress site with hosting designed to
+                  perform. From automated updates and backups to
+                  industry-leading load times, this is WordPress how it was
+                  meant to be.
+                </p>
+              </div>
+              <div className='content-btn'>
+                <button className='btn btn-light btn-content'>
+                  <span>See Plans</span>
+                </button>
+              </div>
+            </div>
           </div>
-          <div className='content-btn'>
-            <button className='btn btn-light btn-content'>
-              <span>See Plans</span>
-            </button>
+          <div className='item'>
+            <div className='image'>
+              <img
+                className='img-fluid'
+                src={ProImage}
+                alt='GoDaddy Pro Program'
+              />
+            </div>
+            <div className='content-wrapper'>
+              <h2 className='title'>GoDaddy Pro Program</h2>
+              <h3 className='sub-title'>
+                Free tools for designers and developers.
+              </h3>
+              <div className='content'>
+                <p>
+                  Save hours with bulk WordPress updates and automated backups,
+                  manage multiple clients from a single dashboard, get exclusive
+                  members-only discounts and more — all for free.
+                </p>
+              </div>
+              <div className='content-btn'>
+                <button className='btn btn-light btn-content'>
+                  <span>Learn More</span>
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className='item'>
-        <div className='image'>
-          <img
-            className='img-fluid'
-            src='https://drive.google.com/uc?export=view&id=1Pj4Ifx4qaMAijiClMlUi8ua7MQkia7v6'
-            alt='GoDaddy Pro Program'
-          />
-        </div>
-        <div className='content-wrapper'>
-          <h2 className='title'>GoDaddy Pro Program</h2>
-          <h3 className='sub-title'>
-            Free tools for designers and developers.
-          </h3>
-          <div className='content'>
-            <p>
-              Save hours with bulk WordPress updates and automated backups,
-              manage multiple clients from a single dashboard, get exclusive
-              members-only discounts and more — all for free.
-            </p>
+          <div className='item'>
+            <div className='image'>
+              <img
+                className='img-fluid'
+                src={ResellerImage}
+                alt='GoDaddy Reseller Program'
+              />
+            </div>
+            <div className='content-wrapper'>
+              <h2 className='title'>GoDaddy Reseller Program</h2>
+              <h3 className='sub-title'>
+                Start your business with our products.
+              </h3>
+              <div className='content'>
+                <p>
+                  Our Reseller Program lets you open your own online business in
+                  a matter of hours. You choose which GoDaddy products you want
+                  to sell — and for how much — and we provide the rest, from the
+                  web store to payment processing.
+                </p>
+              </div>
+              <div className='content-btn'>
+                <button className='btn btn-light btn-content'>
+                  <span>Choose Your Plan</span>
+                </button>
+              </div>
+            </div>
           </div>
-          <div className='content-btn'>
-            <button className='btn btn-light btn-content'>
-              <span>Learn More</span>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className='item'>
-        <div className='image'>
-          <img
-            className='img-fluid'
-            src='https://drive.google.com/uc?export=view&id=1ABXtDlBPicZjZw4CeRSQhBv8Ubr5YHMh'
-            alt='GoDaddy Reseller Program'
-          />
-        </div>
-        <div className='content-wrapper'>
-          <h2 className='title'>GoDaddy Reseller Program</h2>
-          <h3 className='sub-title'>Start your business with our products.</h3>
-          <div className='content'>
-            <p>
-              Our Reseller Program lets you open your own online business in a
-              matter of hours. You choose which GoDaddy products you want to
-              sell — and for how much — and we provide the rest, from the web
-              store to payment processing.
-            </p>
-          </div>
-          <div className='content-btn'>
-            <button className='btn btn-light btn-content'>
-              <span>Choose Your Plan</span>
-            </button>
-          </div>
-        </div>
-      </div> */}
         </div>
       )}
     </>
